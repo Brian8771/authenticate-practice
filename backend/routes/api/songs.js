ffab346e8fb7eb17651219c51f6ae83073809ea0
Brian8771 <brian8771@gmail.com>
2022-07-05 21:32:34 -0500

    got all songs for current users endpoint working

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {restoreUser} = require('../../utils/auth');
 const {requireAuth} = require('../../utils/auth');
 const {Song} = require('../../db/models');
 const {check} = require('express-validator');
@@ -8,6 +8,15 @@ const router = express.Router();
 
 router.get('/', async(req, res) => {
     const songs = await Song.findAll();
+
+    res.json(songs);
+})
+
+router.get('/current', restoreUser, async(req, res) => {
+    const {id}= req.user
+    const songs = await Song.findAll({
+        where: {userId: id}
+    });
 
     res.json(songs);
 })
