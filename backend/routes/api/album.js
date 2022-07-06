f6e9d49b8dceb8ea6a5423e6a25f1c55523056bd
Brian8771 <brian8771@gmail.com>
2022-07-06 12:51:12 -0500

    finished create a song

---
+++ backend/routes/api/album.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const {restoreUser} = require('../../utils/auth');
+const {requireAuth} = require('../../utils/auth');
+const {Song, Album, User} = require('../../db/models');
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+const router = express.Router();
+
+const validateTitleAndUrl = [
+    check('title')
+    .not()
+    .isEmpty()
+    .withMessage('Song title is required'),
+    check('url')
+    .not()
+    .isEmpty()
+    .withMessage('Audio is required'),
+    handleValidationErrors
+];
+
+router.post('/:albumId/songs',[requireAuth, restoreUser, validateTitleAndUrl], async(req, res) => {
+    const {id} = req.user;
+
+    const {title, description, url, imageUrl} = req.body;
+
+    const album = await Album.findOne({where: {id: req.params.albumId}})
+
+
+    if (!album) {
+        res.status(404);
+        res.json({
+            message: "Album couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    if (album.userId !== id) {
+        res.status(403);
+        res.json({
+            message: 'Forbidden',
+            statusCode: 403
+        });
+    }
+
+    const newSong = await Song.create({
+        userId: id,
+        albumId: req.params.albumId,
+        title: title,
+        description,
+        url,
+        previewImage: imageUrl
+    });
+
+    const foundSong = await Song.findOne({where: {title: title}});
+    // if (title) album.title = title;
+    // if (description) album.description = description;
+    // if (url) album.url = url;
+    // if (imageUrl) album.previewImage = imageUrl;
+
+    res.json(foundSong);
+})
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const songsRouter = require('./songs.js');
+const albumsRouter = require('./album.js');
 const {restoreUser} = require('../../utils/auth.js');
 router.use(restoreUser);
 
@@ -9,7 +10,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/songs', songsRouter)
+router.use('/songs', songsRouter);
+
+router.use('/albums', albumsRouter);
 
 router.post('/test', (req, res) => {
     res.json({requestBody: req.body});
