ccd295a2698f967d8863d6013c61945e2dc959ef
Brian8771 <brian8771@gmail.com>
2022-07-07 10:51:09 -0500

    got addings a song to playlist working

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,7 @@ const songsRouter = require('./songs.js');
 const albumsRouter = require('./album.js');
 const commentsRouter = require('./comments.js');
 const artistsRouter = require('./artists.js');
+const playlistsRouter = require('./playlists.js');
 const {restoreUser} = require('../../utils/auth.js');
 router.use(restoreUser);
 
@@ -19,6 +20,8 @@ router.use('/albums', albumsRouter);
 router.use('/comments', commentsRouter);
 
 router.use('/artists', artistsRouter);
+
+router.use('/playlists', playlistsRouter);
 
 router.post('/test', (req, res) => {
     res.json({requestBody: req.body});
---
+++ backend/routes/api/playlists.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const {restoreUser} = require('../../utils/auth');
+const {requireAuth} = require('../../utils/auth');
+const {Song, Album, User, Playlist, PlaylistSong} = require('../../db/models');
+const {check} = require('express-validator');
+const {handleValidationErrors} = require('../../utils/validation');
+const router = express.Router();
+
+router.post('/:playlistId/songs', [requireAuth, restoreUser], async(req, res) => {
+    const {id} = req.user;
+    const {songId} = req.body;
+
+    const playlist = await Playlist.findOne({where: {id: req.params.playlistId}});
+    const song = await Song.findOne({where: {id: songId}});
+
+    if (!playlist) {
+        res.status(404);
+        res.json({
+                message: "Playlist couldn't be found",
+                statusCode: 404
+        })
+    }
+    if (!song) {
+        res.status(404);
+        res.json({
+            message: "Song couldn't be found",
+            statusCode: 404
+          });
+    }
+
+    if (playlist.userId !== id) {
+        res.status(403);
+        return res.json({
+            message: 'Forbidden',
+            statusCode: 403
+        });
+    }
+    let playlists = await PlaylistSong.findOne({where: {playlistId: req.params.playlistId, songId: songId}})
+    if (playlists) {
+        return res.json("Song exists in playlist")
+    }
+    else {
+        await PlaylistSong.create({
+            playlistId: req.params.playlistId,
+            songId: songId
+        });
+
+    }
+
+
+    const playlistSong = await PlaylistSong.findOne({where: {playlistId: req.params.playlistId, songId: songId}})
+    // const playlists = await Playlist.findAll({
+    //     include: [{model: Song}
+
+    //                 ],
+    //                 attributes: {},
+    //                 where: {id: req.params.playlistId}
+    // })
+    res.json({
+        playlistSong,
+        //  playlists
+});
+})
+
+module.exports = router;
