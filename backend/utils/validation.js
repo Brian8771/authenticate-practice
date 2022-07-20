33191e367b5ce5d9a14ebaa88c7e72696b4c91e9
Brian8771 <brian8771@gmail.com>
2022-07-20 17:02:54 -0500

    fixed  validation errors to show as object instead of an array

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,29 +1,30 @@
 const {validationResult} = require('express-validator');
 
+
 const handleValidationErrors = (req, _res, next) => {
-    const validationErrors = validationResult(req);
+  const validationErrors = validationResult(req);
 
-    if (!validationErrors.isEmpty()) {
-      const errors = validationErrors
-        .array()
-        .map((error) => `${error.msg}`);
+  if (!validationErrors.isEmpty()) {
+    const errors = Object.assign({}, ...validationErrors
+      .array()
+      .map((error) => ({[error.param]: error.msg})));
 
-      const err = Error('Validation Error');
-      err.status = 400;
-      err.errors = errors;
-      // err.title = 'Validation Error'
-      next(err);
-    }
-    next();
-  };
+    const err = Error('Validation Error.');
+    err.errors = errors;
+    err.status = 400;
+
+    next(err);
+  }
+  next();
+};
 
 const handleRepeats = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validationErrors
+    const errors = Object.assign({}, ...validationErrors
       .array()
-      .map((error) => `${error.msg}`);
+      .map((error) => ({[error.param]: error.msg})));
 
     const err = Error('User already exists');
     err.status = 403;
