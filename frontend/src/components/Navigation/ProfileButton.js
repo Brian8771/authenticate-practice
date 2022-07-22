740a9fe3557b9a3f79c24d7eb6d6ac127e57c15b
Brian8771 <brian8771@gmail.com>
2022-07-22 18:03:23 -0500

    changed package.json for heroku

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 
+
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
