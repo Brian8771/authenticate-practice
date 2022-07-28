90b503f2a9af547cc20f049cab875a3a82284eb3
Brian8771 <brian8771@gmail.com>
2022-07-28 15:18:05 -0500

    added some css to buttons in navbar

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import {Modal} from '../../context/Modal';
 import LoginForm from './LoginForm';
+import './loginIndex.css'
 
 function LoginFormModal() {
     const [showModal, setShowModal] = useState(false);
 
     return (
         <>
-        <button style={{ fontSize: '14px', backgroundColor: '#333333', border: 'solid 1.5px #4c4c4c', color: '#ffffff', padding: '4px 10px', borderRadius: '2px'}} onClick={() => setShowModal(true)}>Sign In</button>
+        <button className='loginButton'  onClick={() => setShowModal(true)}>Sign In</button>
         {showModal && (
             <Modal onClose={() => setShowModal(false)}>
                 <LoginForm />
