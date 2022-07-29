62f67e8f3065e28869827f86b153087d8daac218
Brian8771 <brian8771@gmail.com>
2022-07-29 01:25:47 -0500

    fixed search input

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -83,6 +83,7 @@ function Navigation({isLoaded}) {
             <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                 <form className='searchForm' onSubmit={handleSubmit}>
                     <input
+                    autoComplete='off'
                     placeholder='Search for song by name'
                     className='searchInput'
                     name='searchBar'
